282f80e1c716797a8155ab6a2884aeb00f94834f
Marek Tlacbaba <marek.tlacbaba@gmail.com>
2018-05-13 23:06:53 +0800

    added simple smart contract with tests to track items from farm via warehouse to retail

--- test/testsGrowNYC.js
+++ test/testsGrowNYC.js
@@ -7,12 +7,45 @@ const should = require('chai')
   .use(require('chai-bignumber')(BigNumber))
   .should();
 
-contract('GrowNYC', function ([]) {
-  
+const metadata = '{ name: apple, weight: 1000}';
+const code = '12234KDS';
+
+contract('GrowNYC', function ([farm, warehouse, retail, customer]) {
+
   beforeEach(async function () {
     this.growNYC = await GrowNYC.new();
   });
 
   it("..deployment successful", async function () {
   });
+
+  it("farm: event is fired correctly", async function () {
+    let tx = await this.growNYC.farm(code, metadata, { from: farm });
+    tx.logs.length.should.be.equal(1);
+    let event = tx.logs[0];
+    event.event.should.be.equal("OnFarm");
+    web3.toUtf8(event.args.code).should.be.equal(code);
+    event.args.user.should.be.equal(farm);
+    event.args.metadata.should.be.equal(metadata);
+  });
+
+  it("warhouse: event is fired correctly", async function () {
+    let tx = await this.growNYC.warehouse(code, metadata, { from: warehouse });
+    tx.logs.length.should.be.equal(1);
+    let event = tx.logs[0];
+    event.event.should.be.equal("InWarehouse");
+    web3.toUtf8(event.args.code).should.be.equal(code);
+    event.args.user.should.be.equal(warehouse);
+    event.args.metadata.should.be.equal(metadata);
+  });
+
+  it("retail: event is fired correctly", async function () {
+    let tx = await this.growNYC.retail(code, metadata, { from: retail });
+    tx.logs.length.should.be.equal(1);
+    let event = tx.logs[0];
+    event.event.should.be.equal("InRetail");
+    web3.toUtf8(event.args.code).should.be.equal(code);
+    event.args.user.should.be.equal(retail);
+    event.args.metadata.should.be.equal(metadata);
+  })
 });
